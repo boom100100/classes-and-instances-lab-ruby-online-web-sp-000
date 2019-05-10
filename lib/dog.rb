6d30e7b32b614e7ab1751e3ca504653c94ca2fcb
Bernadette Davis <bsd231@nyu.edu>
2019-05-10 00:52:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,9 @@ class Dog
 end
 
 fido = Dog.new
+<<<<<<< HEAD
 snoopy = Dog.new
+=======
+snoopie = Dog.new
+>>>>>>> 0a1b9afc311623d3057935737da21d4e21921172
 lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,8 @@
 class Person
 end
+<<<<<<< HEAD
 
 adele_goldberg = Person.new
 alan_kay = Person.new
+=======
+>>>>>>> 0a1b9afc311623d3057935737da21d4e21921172
